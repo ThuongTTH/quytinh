1543d4c2471cda09ee173a42bd5d0ca697a68760
ThuongTTH <huyenthuongtran2502@gmail.com>
2024-07-09 15:29:02 +0700

    add 4 file

---
+++ xoa_phieunhap.php
@@ -0,0 +1,12 @@
+<?php
+    $id=$_GET['id'];
+    //Kết nối đến DB
+    $con=mysqli_connect('localhost','root','','baitaplon')
+    or die('Lỗi kết nối');
+    //Tạo truy vấn xóa
+    $sql="DELETE FROM phieunhap WHERE id='$id'";
+    $kq=mysqli_query($con,$sql);
+    if($kq) echo "<script>alert('Xóa thành công!')</script>";
+    else echo "<script>alert('Xóa thất bại!')</script>";
+    echo "<script>window.location.href='./phieunhap.php'</script>"
+?>
